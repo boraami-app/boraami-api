a095ae99d1467fc1f24008c69c2987c2a94c4540
hodanahmed <hodanahmed00@gmail.com>
2023-05-07 13:55:12 +0100

    repo-setup: added linter

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+const favoriteFruits: string[] = ["apple", "strawberry", "orange"];
+
+function addFruit(fruit: string) {  favoriteFruits.push(fruit); console.log(favoriteFruits);
+}
+
+addFruit("kiwi");
